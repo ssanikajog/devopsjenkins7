3c6a62274197a1ad3fb46b85ff4063382ad67f2b
ssanikajog <sanikajog1296@gmail.com>
2025-11-27 14:10:24 +0530

    Update stu_eval.py

--- stu_eval.py
+++ stu_eval.py
@@ -5,34 +5,23 @@ if len(sys.argv) == 6:
   s3 = sys.argv[3]
   s4 = sys.argv[4]
   s5 = sys.argv[5]
-  avg = (s1+s2+s3+s4+s5)/5
-  print("Average marks: ", avg)
-  if avg > 90 and avg < 100:
-    print("Grade A")
-  elif avg > 80 and avg < 90:
-    print("Grade B")
-  elif avg > 70 and avg < 80:
-    print("Grade C")
-  elif avg > 60 and avg < 70:
-    print("Grade D")
-  else:
-    print("Fail")
-
 else:
   s1 = 80
   s2 = 76
   s3 = 91
   s4 = 80
   s5 = 89
-  avg = (s1+s2+s3+s4+s5)/5
-  print("Average marks: ", avg)
-  if avg > 90 and avg < 100:
-    print("Grade A")
-  elif avg > 80 and avg < 90:
-    print("Grade B")
-  elif avg > 70 and avg < 80:
-    print("Grade C")
-  elif avg > 60 and avg < 70:
-    print("Grade D")
-  else:
-    print("Fail")
+
+avg = (s1+s2+s3+s4+s5)/5
+print("Average marks: ", avg)
+
+if avg > 90 and avg < 100:
+  print("Grade A")
+elif avg > 80 and avg < 90:
+  print("Grade B")
+elif avg > 70 and avg < 80:
+  print("Grade C")
+elif avg > 60 and avg < 70:
+  print("Grade D")
+else:
+  print("Fail")
